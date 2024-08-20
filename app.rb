5fb27bc27d34773fbb4e58748595458e128f0a55
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-08-20 19:59:15 +0000

    modified:   app.rb 	modified:   views/home.erb 	deleted:    views/pairs.erb 	new file:   views/step_one.erb 	new file:   views/step_two.erb

--- app.rb
+++ app.rb
@@ -4,16 +4,49 @@ require "http"
 
 get("/") do
 
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
 
-  @raw_response = HTTP.get(api_url)
 
-  @raw_string = @raw_response.to.s
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch('EXCHANGE_RATE_KEY')}")
+
+  @raw_string = @raw_response.to_s
 
   @parsed_data = JSON.parse(@raw_string)
+
+  @currencies = @parsed_data.fetch("currencies")
+
   erb(:home)
+
+
 end
 
 get("/:from_currency") do
-  @original_currency = params.fetch("from_currecny")
+  @the_symbol = params.fetch("from_currency")
+
+  
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch('EXCHANGE_RATE_KEY')}")
+
+  @raw_string = @raw_response.to_s
+
+  @parsed_data = JSON.parse(@raw_string)
+
+  @currencies = @parsed_data.fetch("currencies")
+
+  erb(:step_one)
+end
+
+get("/:from_currency/:to_currency") do
+  @from = params.fetch("from_currency")
+  @to = params.fetch("to_currency")
+
+  @url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch('EXCHANGE_RATE_KEY')}&from=#{@from}&to=#{@to}&amount=1"
+
+  @raw_response = HTTP.get(@url)
+
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+
+  @amount = @parsed_response.fetch("result")
+
+  erb(:step_two)
 end
